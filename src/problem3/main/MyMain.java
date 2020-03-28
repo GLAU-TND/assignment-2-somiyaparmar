aa138fe050283c829f35f436585ddfe21f3d300a
somiyaparmar <somiya.parmar_cs18@gla.ac.in>
2020-03-28 20:27:20 +0530

    full programme

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,3 +1,4 @@
+
 /*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
@@ -5,7 +6,19 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("somiya",8);
+        obj.insert("kaira",9);
+        obj.insert("yashi",7);
+        obj.insert("rohan",6);
+        obj.insert("raj",1);
+        obj.display();
+    }
 }
